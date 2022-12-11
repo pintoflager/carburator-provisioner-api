9153b3ca207a2403fc567ed75ea080f41fb834d7
pintoflager <jaakko0606@gmail.com>
2022-12-11 18:41:11 -0500

    init scripts for integrations, project install

--- install.sh
+++ install.sh
@@ -3,5 +3,5 @@
 # Curl is required.
 if ! carburator fn integration-installed curl; then
   carburator print terminal error "Missing required program curl. Please install it" \
-    "before running this script." && exit 1
+    "before running this script." && exit 120
 fi
